2b35165d2fbd9f43a3cd579c874f6b0b9b54084f
heri-code <53439194+heri-code@users.noreply.github.com>
2019-11-30 21:59:34 +0700

    push
    kdf

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
-self.addEventListener('notificationclick', function (event) {
-    if (!event.action) {
-      // Penguna menyentuh area notifikasi diluar action
-      console.log('Notification Click.');
-      return;
+self.addEventListener('push', function(event) {
+  var body;
+  if (event.data) {
+    body = event.data.text();
+  } else {
+    body = 'Push message no payload';
+  }
+  var options = {
+    body: body,
+    icon: 'img/notification.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1
     }
-    switch (event.action) {
-      case 'yes-action':
-        console.log('Pengguna memilih action yes.');
-        // buka tab baru
-        clients.openWindow('https://google.com');
-        break;
-      case 'no-action':
-        console.log('Pengguna memilih action no');
-        break;
-      default:
-        console.log(`Action yang dipilih tidak dikenal: '${event.action}'`);
-        break;
-    }
-  });
+  };
+  event.waitUntil(
+    self.registration.showNotification('Push Notification', options)
+  );
+});
